ca6cf538497ac23a5b0c853b5a07f19a8ab33ee5
ShadowRonin <shadowronin42@gmail.com>
2023-11-16 10:45:05 -0500

    add openapi

--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,82 @@ use eyre::Result;
 use tokio::time::{sleep, Duration};
 use serde::{Serialize, Deserialize};
 
-
 use poem::{
-    get,
-    post,
-    handler, 
     listener::TcpListener, 
-    web::Path, 
     Route, 
-    Server, 
-    web::Json
 };
+
+use poem_openapi::{
+    OpenApi, 
+    OpenApiService,
+    payload::Json,
+    Object,
+    param::Path
+};
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/todo", method = "post")]
+    async fn create_todo(&self, Json(todo): Json<Todo>) {
+        sleep(Duration::from_millis(100)).await;
+    
+        println!("New todo: {:?}", todo);
+    }
+
+    #[oai(path = "/todo/:id", method = "get")]
+    async fn get_todo(&self, Path(id): Path<u64>) -> Json<Todo> {
+        sleep(Duration::from_millis(100)).await;
+    
+        Json(Todo {
+            id: 1,
+            title: "Example".to_owned(),
+            description: "this is an example".to_owned()
+        })
+    }
+
+    #[oai(path = "/todo", method = "get")]
+    async fn get_todos(&self) -> Json<Vec<Todo>> {
+        sleep(Duration::from_millis(100)).await;
+    
+        Json(vec!(
+            Todo {
+                id: 1,
+                title: "Example".to_owned(),
+                description: "this is an example".to_owned()
+            },
+            Todo {
+                id: 2,
+                title: "Example 2".to_owned(),
+                description: "this is another example".to_owned()
+            }
+        ))
+    }
+}
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // make run time errors colorful
     color_eyre::install()?;
 
+    let api_service = OpenApiService::new(Api, "Manage Todos", "1.0").server("http://127.0.0.1:3000/api");
+    let ui = api_service.openapi_explorer();
     let app = Route::new()
-    .at("/todo/:id", get(get_todo))
-    .at("/todo/create", post(get_todo))
-    .at("/todo", get(get_todos));
-    Server::new(TcpListener::bind("127.0.0.1:3000"))
-      .run(app)
-      .await?;
+        .nest("/api", api_service)
+        .nest("/", ui);
+
+    poem::Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await?;
 
     Ok(())
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Object, Serialize)]
 struct Todo {
     id: u64,
     title: String,
     description: String,
-}
-
-#[handler]
-async fn create_todo(Json(todo): Json<Todo>) {
-    sleep(Duration::from_millis(100)).await;
-
-    println!("New todo: {:?}", todo);
-}
-
-#[handler]
-async fn get_todos() -> Json<Vec<Todo>> {
-    sleep(Duration::from_millis(100)).await;
-
-    Json(vec!(
-        Todo {
-            id: 1,
-            title: "Example".to_owned(),
-            description: "this is an example".to_owned()
-        },
-        Todo {
-            id: 2,
-            title: "Example 2".to_owned(),
-            description: "this is another example".to_owned()
-        }
-    ))
-}
-
-#[handler]
-async fn get_todo(Path(name): Path<u64>) -> Json<Todo> {
-    sleep(Duration::from_millis(100)).await;
-
-    Json(Todo {
-        id: 1,
-        title: "Example".to_owned(),
-        description: "this is an example".to_owned()
-    })
 }
